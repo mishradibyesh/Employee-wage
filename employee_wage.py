6d0ae461fb72ae32107ece1eead6a6d325108f5a
mishradibyesh <dibyeshmishra207@gmail.com>
2021-12-15 20:04:12 +0530

    ADDED : code to have Part time employee and wage

--- employee_wage.py
+++ employee_wage.py
@@ -3,12 +3,20 @@ Developing employee Wage Computation Application.
 calculating daily wage if employee is present
 """
 import random
-is_present = random.randint(0,2)
 salary = 0
-if is_present == 1:
-	full_day_hour = 8
-	wage_per_hour = 20
-	salary = salary + (wage_per_hour * full_day_hour)
-	print(f"Employee is present and his today's income is {salary}")
+FULL_TIME_HOUR = 8
+PART_TIME_HOUR = 4
+WAGE_PER_HOUR = 20
+employee_hour = 0
+emp_type = random.randint(0,3)
+if emp_type == 1:
+	employee_hour = FULL_TIME_HOUR
+	print("Employee is Full time ")
+elif emp_type == 2:
+	employee_hour = PART_TIME_HOUR
+	print("Employee is Part time ")
 else:
-	print(f"Employee is absent and  his today's income is {salary}")
+	print("Employee is absent ")
+	employee_hour = 0
+salary = salary + (WAGE_PER_HOUR * employee_hour)
+print(f"Income is {salary}")
