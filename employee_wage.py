e212904f0a17c6989495ec5f5f6d5140db87494c
mishradibyesh <dibyeshmishra207@gmail.com>
2021-12-17 20:59:39 +0530

    REFACTORED : modified the code to calculate wages of different employees of diffrent companies

--- employee_wage.py
+++ employee_wage.py
@@ -10,17 +10,11 @@ import random
 class EmployeeWage:
     FULL_TIME_HOUR = 8
     PART_TIME_HOUR = 4
-    wage_per_hour = 20
-    total_working_day = 20
-    total_working_hours = 100
 
-    def calculate_emp_wage(self , company , wage_per_hour , total_working_day , total_working_hours ):
+    def calculate_emp_wage(self, *employee_data):
         total_employee_hour = 0
         working_day = 0
-        self.total_working_hours = total_working_hours
-        self.total_working_day = total_working_day
-        self.wage_per_hour = wage_per_hour
-        while working_day < self.total_working_day and total_employee_hour <= self.total_working_hours:
+        while working_day < employee_data[2] and total_employee_hour <= employee_data[3]:
             working_day += 1
             emp_type = random.randint(0, 3)
             if emp_type == 1:
@@ -33,13 +27,13 @@ class EmployeeWage:
                 print(f"on day {working_day} Employee is absent ")
                 employee_hour = 0
             total_employee_hour += employee_hour
-            if total_employee_hour > self.total_working_hours:
+            if total_employee_hour > employee_data[3]:
                 total_employee_hour -= employee_hour
                 break
-        salary = self.wage_per_hour * total_employee_hour
+        salary = employee_data[1] * total_employee_hour
         print(f"Monthly Income is {salary}")
         print(f"Total working Hours is {total_employee_hour}")
-        employee_data_list = [salary , company]
+        employee_data_list = [salary , employee_data[0]]
         return employee_data_list
 
     def print_details(self, *data):
@@ -47,7 +41,7 @@ class EmployeeWage:
 
 
 employee1 = EmployeeWage()
-data_list = employee1.calculate_emp_wage("amazon" , 23 , 25 , 90)
+data_list = employee1.calculate_emp_wage("amazon", 23, 25, 90)
 employee1.print_details(data_list)
 employee2 = EmployeeWage()
 data_list = employee2.calculate_emp_wage("flipkart" , 22 , 24 , 80)
