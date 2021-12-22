e8e293991c3bb0e888b4a9241d02559a46095cbe
mishradibyesh <dibyeshmishra207@gmail.com>
2021-12-22 12:07:25 +0530

    REFACTORED : code refactored to seach the monthly wage by company name

--- employee_builder.py
+++ employee_builder.py
@@ -1,16 +1,22 @@
-from  employee_wage import EmployeeWageCalculator
-from  custom_exception import WrongInputException
+from custom_exception import WrongInputException
 
 
-class EmployeeBuilder(EmployeeWageCalculator):
+class Company:
     """
+    initiating the class members by init method
     using instance variable instead of function parameter
     storing daily wage of the employee
     searching and displaying monthly employee wage by company  name
     """
     employee_details_list = []
 
-    def add_employee_details(self , employee):
+    def __init__(self, company, wage_per_hour, total_working_day, total_working_hours):
+        self.company = company
+        self.wage_per_hour = wage_per_hour
+        self.total_working_day = total_working_day
+        self.total_working_hours = total_working_hours
+
+    def add_employee_details(self, employee):
         """
             desc: to add the details in a list 
             param: employee instance variable
@@ -18,7 +24,7 @@ class EmployeeBuilder(EmployeeWageCalculator):
         """
         self.employee_details_list.append(employee)
 
-    def query_by_company_name(self ):
+    def query_by_company_name(self):
         """
             desc: to print the Details by Searching with company name
             param: employee_details_list
@@ -27,7 +33,7 @@ class EmployeeBuilder(EmployeeWageCalculator):
         company_name = input("enter the company name : ")
         count = 0
         for i in range(len(self.employee_details_list)):
-            if company_name.lower() == (self.employee_details_list[i].company):
+            if company_name.lower() == self.employee_details_list[i].company:
                 print("Monthly salary : "+str(self.employee_details_list[i].salary))
                 count = count + 1
                 break
@@ -39,17 +45,3 @@ class EmployeeBuilder(EmployeeWageCalculator):
         display  company name , daily wage and total wage of employees
         """
         print(f"{self.employee_details_list[0]}  " + f"{self.employee_details_list[1]}")
-
-
-employee1 = EmployeeBuilder("amazon", 23, 25, 90)
-employee1.calculate_emp_wage()
-employee1.add_employee_details(employee1)
-employee2 = EmployeeBuilder("flipkart", 22, 24, 80)
-employee2.calculate_emp_wage()
-employee2.add_employee_details(employee2)
-employee2.display_details()
-
-try:
-    employee2.query_by_company_name()
-except WrongInputException as exception :
-    print (exception)
